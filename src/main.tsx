4ba971ab5f8cc6e429198bcbf06663e416f43218
Rastraponovich <wilde@bk.ru>
2023-06-29 16:52:17 +0300

    user-settings page fix html

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import ReactDOM from "react-dom/client";
 import React from "react";
 
 import "app/styles/index.css";
-import { OnboardingPage } from "./pages/workspace/onboarding";
+import { UserPage } from "./pages/user/settings";
 
 const root = document.getElementById("root") as HTMLElement;
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <OnboardingPage />
+    <UserPage />
   </React.StrictMode>
 );
