1a4df85b2edb42c8853909102eaf9b3429e75aa8
Rastrapon <58354560+Rastraponovich@users.noreply.github.com>
2023-06-29 14:26:21 +0300

    Bug/auth page (#27)
    * rework social-auth-button
    
    * added hover and focus styles
    
    * fixes grid

--- src/widgets/layout/ui/index.tsx
+++ src/widgets/layout/ui/index.tsx
@@ -8,9 +8,17 @@ export const Layout = ({ children }: ILayoutProps) => {
   return (
     <>
       <Header />
-      <main className="container mx-auto my-0 flex grow flex-col gap-8 overflow-y-hidden pb-12 pt-8  sm:pb-12 sm:pt-12">
-        {children}
-      </main>
+      <BaseLayout>{children}</BaseLayout>
     </>
+  );
+};
+interface IBaseLayoutProps {
+  children: ReactNode;
+}
+export const BaseLayout = ({ children }: IBaseLayoutProps) => {
+  return (
+    <main className="container mx-auto my-0 flex grow flex-col gap-8 overflow-y-hidden pb-12 pt-8  sm:pb-12 sm:pt-12">
+      {children}
+    </main>
   );
 };
