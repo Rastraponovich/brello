d564f90e83e206ec61bc4e80e9f853d59baa9dbe
Rastraponovich <wilde@bk.ru>
2023-07-03 16:25:34 +0300

    input change styles

--- src/shared/ui/input/ui/index.tsx
+++ src/shared/ui/input/ui/index.tsx
@@ -1,17 +1,13 @@
 import clsx from "clsx";
 import { memo } from "react";
-import {
-  CAPTION_POSITION_DICT,
-  ECaptionPosition,
-  EInputSize,
-  INPUT_SIZE_DICT,
-} from "../lib/helpers";
-import { models } from "../lib";
+
+import { type models, helpers } from "../lib";
+import { BaseIcon } from "shared/ui/icon";
 
 export const Input = memo<models.IInputProps>(
-  ({ caption, captionPosition, className, ...props }) => {
+  ({ caption, className, hint, ...props }) => {
     return (
-      <InputWrapper caption={caption} captionPosition={captionPosition}>
+      <InputWrapper caption={caption} hint={hint}>
         <BaseInput {...props} className={clsx(className)} />
       </InputWrapper>
     );
@@ -19,16 +15,22 @@ export const Input = memo<models.IInputProps>(
 );
 
 const BaseInput = memo<models.IBaseInput>(
-  ({ className, size = EInputSize.MD, ...props }) => {
+  ({ className, size = helpers.EInputSize.MD, ...props }) => {
     return (
       <input
         {...props}
         data-qa="Input__value"
         className={clsx(
-          "text-base font-normal focus:text-gray-900",
-          "border border-gray-300 focus-visible:border-blue-300 focus-visible:shadow-md focus-visible:ring-4 focus-visible:ring-blue-100",
-          "gap-2 rounded-lg  bg-white text-gray-500 shadow-sm placeholder:text-gray-500 ",
-          INPUT_SIZE_DICT[size],
+          "text-base text-gray-900 placeholder:text-gray-500",
+          "gap-2 bg-white shadow-sm",
+          "border-gray-300, rounded-lg border",
+          "focus-visible:outline-blue-300 focus-visible:ring-4 focus-visible:ring-blue-100",
+          "focus:outline-blue-300 focus:ring-4 focus:ring-blue-100",
+          "focus-within:outline-blue-300 focus-within:ring-4 focus-within:ring-blue-100",
+          "invalid:focus-within:red-100 invalid:focus-within:red-100 invalid:focus:outline-red-300 invalid:focus:ring-red-100",
+          "focus-within:shadow-none focus:shadow-none focus-visible:shadow-none",
+          "disabled:bg-gray-50",
+          helpers.INPUT_SIZE_DICT[size],
           className
         )}
       />
@@ -39,7 +41,12 @@ const BaseInput = memo<models.IBaseInput>(
 export const InputSearch = memo<models.IInputProps>(({ caption, ...props }) => {
   return (
     <InputWrapper caption={caption} className="relative justify-center">
-      <SearchIcon className="absolute left-2 h-5 w-5" />
+      <BaseIcon
+        className="absolute left-2 h-5 w-5"
+        source="general"
+        icon="search"
+        size="normal"
+      />
       <BaseInput
         className={clsx(props.className, "pl-9 pr-3")}
         {...props}
@@ -50,40 +57,45 @@ export const InputSearch = memo<models.IInputProps>(({ caption, ...props }) => {
 });
 
 const InputWrapper = memo<models.IInputWrapper>(
-  ({
-    children,
-    caption,
-    className,
-    captionPosition = ECaptionPosition.Top,
-  }) => {
+  ({ children, caption, className, hint }) => {
     return (
       <label
-        className={clsx(
-          className,
-          "flex gap-1.5",
-          CAPTION_POSITION_DICT[captionPosition]
-        )}
         data-qa="Input__container"
+        className={clsx(
+          "flex flex-col gap-1.5 text-left text-sm font-normal",
+          className
+        )}
       >
         {caption && (
           <span
-            className="self-start text-sm text-gray-700"
-            data-qa="Input__caption"
             title={caption}
+            data-qa="Input__caption"
+            className="font-medium text-gray-700"
           >
             {caption}
           </span>
         )}
         {children}
+        {hint && (
+          <span
+            title={hint?.text}
+            data-qa="Input__hint"
+            className={clsx(
+              hint.type === "invalid" ? "text-rose-500" : "text-gray-600"
+            )}
+          >
+            {hint?.text}
+          </span>
+        )}
       </label>
     );
   }
 );
 
 export const InputArea = memo<models.IInputAreaProps>(
-  ({ caption, captionPosition, className, ...props }) => {
+  ({ caption, className, hint, ...props }) => {
     return (
-      <InputWrapper caption={caption} captionPosition={captionPosition}>
+      <InputWrapper caption={caption} hint={hint}>
         <BaseInputArea {...props} className={clsx(className)} />
       </InputWrapper>
     );
@@ -134,9 +146,9 @@ const BaseInputWeb = memo<models.IBaseInputWeb>(
 BaseInputWeb.displayName = "BaseInputWeb";
 
 export const InputWeb = memo<models.IInputWebProps>(
-  ({ caption, captionPosition, leftPlaceholder, rightPlaceholder }) => {
+  ({ caption, hint, leftPlaceholder, rightPlaceholder }) => {
     return (
-      <InputWrapper caption={caption} captionPosition={captionPosition}>
+      <InputWrapper caption={caption} hint={hint}>
         <BaseInputWeb
           leftPlaceholder={leftPlaceholder}
           rightPlaceholder={rightPlaceholder}
