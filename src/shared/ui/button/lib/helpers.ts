33028096cd1ae747ed081c8676fc97deb567598e
Rastraponovich <wilde@bk.ru>
2023-07-04 14:44:28 +0300

    rework baseButton component to cva

--- src/shared/ui/button/lib/helpers.ts
+++ src/shared/ui/button/lib/helpers.ts
@@ -1,125 +1,159 @@
-import { TButtonSize, TButtonTextAlign, TVariant } from "./models";
+import { cva, cx } from "class-variance-authority";
+import { TButtonSize, TVariant } from "./models";
 
-enum CloseXButtonSize {
-  SM = "p-2",
-  MD = "p-2.5",
-  LG = "p-2.5",
-}
+const buttonBase = cva("", {
+  variants: {
+    variant: {
+      link: "",
+      linkGray: "",
+      primary: "bg-blue-600 text-white hover:bg-blue-700 disabled:bg-blue-200",
+      tertiary:
+        " hover:border-blue-50 hover:bg-blue-50 disabled:border-transparent",
+      tertiaryGray: "",
+      secondary:
+        "border-blue-50 hover:border-blue-100 bg-blue-50 hover:bg-blue-100  disabled:bg-[#F5F8FF] disabled:text-blue-300 disabled:border-[#F5F8FF] focus:outline-blue-100",
+      secondaryGray: "hover:text-gray-800 text-gray-700",
+    },
+  },
+  compoundVariants: [
+    {
+      variant: ["tertiary", "tertiaryGray"],
+      className: "border-transparent",
+    },
+    {
+      variant: ["tertiary", "tertiaryGray", "secondaryGray"],
+      className: "disabled:hover:bg-transparent",
+    },
+    {
+      variant: ["secondaryGray", "tertiaryGray"],
+      className: "hover:bg-gray-50",
+    },
+    {
+      variant: ["link", "linkGray"],
+      className: "focus:outline-none border-none !p-0",
+    },
+    {
+      variant: ["linkGray", "tertiaryGray"],
+      className: "text-gray-600  hover:text-gray-700",
+    },
+    {
+      variant: ["link", "tertiary", "secondary"],
+      className: "text-blue-700 hover:text-blue-800",
+    },
+    {
+      variant: [
+        "secondaryGray",
+        "tertiaryGray",
+        "link",
+        "linkGray",
+        "tertiary",
+      ],
+      className: "disabled:text-gray-300",
+    },
+  ],
+  defaultVariants: {
+    variant: "primary",
+  },
+});
 
-export const CLOSE_BUTTON_SIZE_DICT: Record<
-  "sm" | "md" | "lg",
-  CloseXButtonSize
-> = {
-  sm: CloseXButtonSize.SM,
-  md: CloseXButtonSize.MD,
-  lg: CloseXButtonSize.LG,
-};
+const buttonIconSize = cva("", {
+  variants: {
+    size: {
+      xs: "p-2",
+      sm: "p-2.5",
+      md: "p-3",
+      lg: "p-3.5",
+      xl: "p-4",
+    },
+  },
+  defaultVariants: {
+    size: "sm",
+  },
+});
 
-enum CloseXButtonVariant {
-  PRIMARY = "text-blue-500 hover:bg-blue-50 hover:text-blue-600 focus:bg-blue-50",
-  GRAY = "text-gray-500 hover:bg-gray-50 hover:text-gray-600 focus:bg-gray-50",
-}
+const buttonSize = cva("", {
+  variants: {
+    size: {
+      xs: "py-2 px-3.5",
+      sm: "py-2.5 px-4",
+      md: "py-2.5 px-4.5",
+      lg: "py-3 px-5",
+      xl: "py-4 px-7 text-xl gap-3",
+    },
+  },
+  compoundVariants: [
+    { size: ["xs", "sm"], className: "text-sm" },
+    { size: ["md", "lg"], className: "text-base" },
+    { size: ["xs", "sm", "md", "lg"], className: "gap-2" },
+  ],
+  defaultVariants: {
+    size: "sm",
+  },
+});
 
-export const CLOSE_BUTTON_VARIANT_DICT: Record<
-  "primary" | "gray",
-  CloseXButtonVariant
-> = {
-  primary: CloseXButtonVariant.PRIMARY,
-  gray: CloseXButtonVariant.GRAY,
-};
+/**
+ * @description style generate function for CloseXButton component
+ */
+export const closeXButton = cva(
+  "flex items-center rounded-lg border border-transparent font-semibold",
+  {
+    variants: {
+      size: {
+        sm: "p-2",
+        md: "",
+        lg: "",
+      },
+      variant: {
+        primary:
+          "text-blue-500 hover:bg-blue-50 hover:text-blue-600 focus:bg-blue-50",
+        gray: "text-gray-500 hover:bg-gray-50 hover:text-gray-600 focus:bg-gray-50",
+      },
+    },
+    compoundVariants: [
+      {
+        size: ["md", "lg"],
+        className: "p-2.5",
+      },
+    ],
+    defaultVariants: {
+      size: "sm",
+      variant: "primary",
+    },
+  }
+);
 
-enum ButtonIconSize {
-  XS = "p-2",
-  SM = "p-2.5",
-  MD = "p-3",
-  LG = "p-3.5",
-  XL = "p-4",
-}
+/**
+ * @description style generate function for IconButton component
+ */
+export const iconButton = ({
+  variant,
+  size,
+  className,
+}: {
+  variant: TVariant;
+  size: TButtonSize;
+  className: string | undefined;
+}) =>
+  cx(
+    "flex shrink items-center rounded-lg border font-semibold",
+    buttonBase({ variant, className }),
+    buttonIconSize({ size })
+  );
 
-export const BUTTON_ICON_SIZE: Record<TButtonSize, ButtonIconSize> = {
-  xs: ButtonIconSize.XS,
-  sm: ButtonIconSize.SM,
-  md: ButtonIconSize.MD,
-  lg: ButtonIconSize.LG,
-  xl: ButtonIconSize.XL,
-};
-
-enum ButtonTextSize {
-  XS = "text-sm",
-  SM = "text-sm",
-  MD = "text-base",
-  LG = "text-base",
-  XL = "text-xl",
-}
-export const BUTTON_TEXT_SIZE_DICT: Record<TButtonSize, ButtonTextSize> = {
-  xs: ButtonTextSize.XS,
-  sm: ButtonTextSize.SM,
-  md: ButtonTextSize.MD,
-  lg: ButtonTextSize.LG,
-  xl: ButtonTextSize.XL,
-};
-
-enum ButtonSize {
-  XS = "py-2 px-3.5",
-  SM = "py-2.5 px-4",
-  MD = "py-2.5 px-4.5",
-  LG = "py-3 px-5",
-  XL = "py-4 px-7",
-}
-export const BUTTON_SIZES_DICT: Record<TButtonSize, ButtonSize> = {
-  xs: ButtonSize.XS,
-  sm: ButtonSize.SM,
-  md: ButtonSize.MD,
-  lg: ButtonSize.LG,
-  xl: ButtonSize.XL,
-};
-
-enum ButtonColors {
-  LINK = "text-blue-700 hover:text-blue-800 disabled:text-gray-300",
-  LINK_GRAY = "text-gray-600 disabled:text-gray-300 hover:text-gray-700",
-  PRIMARY = "bg-blue-600 text-white hover:bg-blue-700 disabled:bg-blue-200",
-  TERTIARY = "text-blue-700 hover:bg-blue-50 hover:text-blue-800 disabled:text-gray-300",
-  TERTIARY_GRAY = "text-gray-600 disabled:text-gray-300 hover:bg-gray-50 hover:text-gray-700",
-  SECONDARY = "border-blue-50 text-blue-700 bg-blue-50 hover:bg-blue-100 hover:text-blue-800 disabled:bg-[#F5F8FF] disabled:text-blue-300",
-  SECONDARY_GRAY = "border-gray-300 text-gray-700 bg-white hover:bg-gray-50 hover:text-gray-800 disabled:border-gray-200 disabled:text-gray-300",
-}
-
-export const BUTTON_COLORS: Record<TVariant, ButtonColors> = {
-  link: ButtonColors.LINK,
-  primary: ButtonColors.PRIMARY,
-  tertiary: ButtonColors.TERTIARY,
-  linkGray: ButtonColors.LINK_GRAY,
-  secondary: ButtonColors.SECONDARY,
-  tertiaryGray: ButtonColors.TERTIARY_GRAY,
-  secondaryGray: ButtonColors.SECONDARY_GRAY,
-};
-
-export enum ButtonVariant {
-  LINK = "border-none !p-0",
-  LINK_GRAY = "border-none !p-0",
-  PRIMARY = "border-blue-600 hover:border-blue-700 disabled:border-blue-200",
-  TERTIARY = "border-transparent hover:border-blue-50",
-  TERTIARY_GRAY = "border-transparent",
-  SECONDARY = "hover:border-blue-100",
-  SECONDARY_GRAY = "",
-}
-export const BUTTON_VARIANTS: Record<TVariant, string> = {
-  link: ButtonVariant.LINK,
-  primary: ButtonVariant.PRIMARY,
-  tertiary: ButtonVariant.TERTIARY,
-  linkGray: ButtonVariant.LINK_GRAY,
-  secondary: ButtonVariant.SECONDARY,
-  tertiaryGray: ButtonVariant.TERTIARY_GRAY,
-  secondaryGray: ButtonVariant.SECONDARY_GRAY,
-};
-
-enum ButtonTextAlign {
-  Left = "text-left",
-  Right = "text-right",
-  Center = "text-center",
-}
-export const BUTTON_TEXT_ALIGN: Record<TButtonTextAlign, ButtonTextAlign> = {
-  left: ButtonTextAlign.Left,
-  right: ButtonTextAlign.Right,
-  center: ButtonTextAlign.Center,
-};
+/**
+ * @description style generate function for Button component
+ */
+export const button = ({
+  variant,
+  size,
+  className,
+}: {
+  variant: TVariant;
+  size: TButtonSize;
+  className: string | undefined;
+}) =>
+  cx(
+    "flex shrink items-center rounded-lg border font-semibold justify-center",
+    buttonBase({ variant, className }),
+    buttonSize({ size })
+  );
