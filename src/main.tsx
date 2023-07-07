b187156d7c2647b8cd20d95b8ab0085ebecd486c
Rastraponovich <wilde@bk.ru>
2023-07-07 13:10:38 +0300

    added babel config

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import React from "react";
 
 import "app/styles/index.css";
 import { App } from "./app";
+import { appStarted } from "./shared/config";
 
 const root = document.getElementById("root") as HTMLElement;
+
+appStarted();
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
