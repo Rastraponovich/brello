bdc4b126ec5281d300c691cab4d859695a98a79e
Rastraponovich <wilde@bk.ru>
2023-08-11 12:38:28 +0300

    chore: added router Provider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,24 @@
+import "app/styles/index.css";
+import { RouterProvider } from "atomic-router-react";
 import { allSettled, fork } from "effector";
 import { Provider } from "effector-react";
 import { createRoot } from "react-dom/client";
 
-import "app/styles/index.css";
+import { appStarted } from "~/shared/init";
+import { router } from "~/shared/routing";
 
-import { Application, appStarted } from "./app";
+import { Application } from "./app";
 
 const root = document.getElementById("root") as HTMLElement;
 
 const scope = fork();
 
-allSettled(appStarted, { scope });
+allSettled(appStarted, { scope }).catch(() => console.warn("совсем все плохо"));
 
 createRoot(root).render(
   <Provider value={scope}>
-    <Application />
+    <RouterProvider router={router}>
+      <Application />
+    </RouterProvider>
   </Provider>,
 );
