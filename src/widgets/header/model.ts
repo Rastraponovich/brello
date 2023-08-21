0793f3547809b146d3733f0e314cfa867192a3ba
Rastraponovich <wilde@bk.ru>
2023-08-21 23:41:16 +0300

    chore: nav list added path intersect in model

--- src/widgets/header/model.ts
+++ src/widgets/header/model.ts
@@ -1,11 +1,13 @@
 import { createEvent, createStore } from "effector";
 
-import { $currentPage } from "~/shared/routing";
+import { router } from "~/shared/routing";
 import { TNavItem } from "~/shared/ui/nav-item";
 
 import { __NAVS__ } from "./constants";
 
+const baseUrl = document.location.toString();
+
 export const $menuItems = createStore<TNavItem[]>(__NAVS__);
-export const $selected = createStore<TNavItem["path"]>("/").on($currentPage, (_, path) => path);
+export const $selected = createStore<TNavItem["path"]>(baseUrl).on(router.$path, (_, path) => path);
 
 export const navButtonClicked = createEvent<string>();
