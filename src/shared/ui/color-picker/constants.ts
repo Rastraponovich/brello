adc98ae9f203593c21f248659f6f7fbd75ddca42
Rastraponovich <wilde@bk.ru>
2024-01-06 14:33:10 +0300

    feat: refactor color-picker

---
+++ src/shared/ui/color-picker/constants.ts
@@ -0,0 +1,52 @@
+export const COLORS = [
+  {
+    id: 1,
+    title: "yellow",
+    value: "bg-yellow-500",
+    className: "text-yellow-500 hover:outline-yellow-500",
+  },
+  { title: "red", id: 2, value: "bg-red-500", className: "text-red-500 hover:outline-red-500" },
+  {
+    id: 3,
+    title: "pink",
+    value: "bg-pink-500",
+    className: "text-pink-500 hover:outline-pink-500",
+  },
+  {
+    id: 4,
+    title: "zinc",
+    value: "bg-zinc-500",
+    className: "text-zinc-500 hover:outline-zinc-500",
+  },
+  {
+    id: 5,
+    title: "rose",
+    value: "bg-rose-500",
+    className: "text-rose-500 hover:outline-rose-500",
+  },
+  {
+    id: 6,
+    title: "blue",
+    value: "bg-blue-500",
+    className: "text-blue-500 hover:outline-blue-500",
+  },
+  {
+    id: 7,
+    title: "orange",
+    value: "bg-orange-500",
+    className: "text-orange-500 hover:outline-orange-500",
+  },
+  {
+    id: 8,
+    title: "indigo",
+    value: "bg-indigo-500",
+    className: "text-indigo-500 hover:outline-indigo-500",
+  },
+
+  {
+    id: 9,
+    title: "indigo gradient",
+    value: "bg-gradient-to-r from-purple-500 to-pink-500",
+    className: "text-indigo-500 hover:outline-indigo-500",
+  },
+];
